bca571386ec460475a3d5f2b1cffbf807a12393d
augustoache <127363609+augustoache@users.noreply.github.com>
2023-03-08 23:12:41 -0300

    modifica ruta img

--- function.js
+++ function.js
@@ -1,26 +1,26 @@
 function enColor1() {
     Element =document.getElementById("insta");
-    Element.src="img/insta2.png";
+    Element.src="insta2.png";
 }
 function enNegro1() {
     Element =document.getElementById("insta");
-    Element.src="img/insta.png";
+    Element.src="insta.png";
 }
 
 function enColor2() {
     Element =document.getElementById("face");
-    Element.src="img/face2.png";
+    Element.src="face2.png";
 }
 function enNegro2() {
     Element =document.getElementById("face");
-    Element.src="img/face.png";
+    Element.src="face.png";
 }
 
 function enColor3() {
     Element =document.getElementById("wa");
-    Element.src="img/whatsap2.png";
+    Element.src="whatsap2.png";
 }
 function enNegro3() {
     Element =document.getElementById("wa");
-    Element.src="img/whatsap.png";
+    Element.src="whatsap.png";
 }
